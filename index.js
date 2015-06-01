eaa8d37b6d66b64dd6bbad56738b23cd951609b1
Jeff Federman <jefffederman@gmail.com>
2015-06-01 00:53:40 -0400

    Change dealer button to player actions form to handle dynamic number of players

--- index.js
+++ index.js
@@ -92,9 +92,38 @@ var Winners = React.createClass({
       </div>
     );
   }
-})
+});
 
-var DealButton = React.createClass({
+var PlayerActions = React.createClass({
+  getInitialState: function() {
+    return { playerCount: 2};
+  },
+  createOptions: function() {
+    return [1,2,3,4,5,6,7,8,9].map(function(i) {
+      return (
+        <option key={i}>{i}</option>
+      );
+    });
+  },
+  handlePlayerCountChange: function(e) {
+    this.setState({ playerCount: e.target.value });
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var self = this;
+    // 2015-05-31 Run into CORS preflight/redirect issues if we use JSON data
+    var data = "playerCount=" + this.state.playerCount;
+    $.ajax({
+      method: this.props.httpMethod,
+      url: this.props.action,
+      data: data,
+      dataType: 'json',
+      crossDomain: true,
+      success: function(data, textStatus, jqXHR) {
+        self.renderPokerTable(data, self.showWinners);
+      }
+    });
+  },
   showWinners: function(json) {
     if (json['winners']) {
       React.render(
@@ -103,35 +132,12 @@ var DealButton = React.createClass({
       );
     }
   },
-  handleClick: function() {
-    var xhr = new XMLHttpRequest();
-    var self = this;
-    xhr.onload = function() {
-      if (xhr.status !== 200) {
-        throw new Error('request failed');
-      }
-      var json = JSON.parse(xhr.response);
-      self.renderPokerTable(json, self.showWinners);
-    }
-
-    if (this.props.street == 'preflop') {
-      xhr.open('POST', 'http://localhost:3000/hands', true);
-    } else {
-      xhr.open('GET', 'http://localhost:3000/hands/' + this.props.handId + '/' + this.props.street, true)
-    }
-
-    xhr.send();
-  },
-  render: function() {
-    return (
-      <button onClick={this.handleClick}>Deal {this.props.street}</button>
-    );
-  },
   renderPokerTable: function(json, callback) {
     var holeCards = json.players.map(function(p) { return p['holeCards']; });
     React.unmountComponentAtNode(document.getElementById('winners'));
     React.render(
-      <DealButton street={json['street']} handId={json['handId']}/>, document.getElementById('deal-button')
+      <PlayerActions action={json['formAction']} street={json['street']} handId={json['handId']} httpMethod={json['httpMethod']} />,
+      document.getElementById('deal-button')
     );
     React.render(
       <PokerTable holeCards={holeCards} boardCards={json['board']} />,
@@ -140,14 +146,26 @@ var DealButton = React.createClass({
     if (typeof callback === 'function') {
       callback(json);
     }
+  },
+  render: function() {
+    return(
+      <form onSubmit={this.handleSubmit} action={this.props.action} method={this.props.httpMethod}>
+        <select name='playerCount' onChange={this.handlePlayerCountChange} value={this.state.playerCount}>
+          {this.createOptions()}
+        </select>
+        <button>Deal {this.props.street}</button>
+      </form>
+    );
   }
 });
 
 // Init
-React.render(
-  <DealButton street='preflop'/>, document.getElementById('deal-button')
-);
 
 React.render(
   <PokerTable boardCards={[]} holeCards={[]} />, document.getElementById('poker-table')
+);
+
+React.render(
+  <PlayerActions action='http://localhost:3000/hands/' street='preflop' httpMethod='POST' />,
+  document.getElementById('deal-button')
 );
